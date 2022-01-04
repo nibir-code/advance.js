d84dcee9da6dec7d72bdcfd924f62094f14dbd56
nibir-code <mubtashimfuadnibir@gmail.com>
2022-01-05 00:48:46 +0600

    first ever  commit

---
+++ true & false.js
@@ -0,0 +1,13 @@
+// falsy
+//0
+// undefined
+//null
+//NaN
+//truthy
+let name = 12;
+if(name || name== 0){
+     console.log("true")
+}
+else{
+     console.log("false")
+}
---
+++ undefined & null.js
@@ -0,0 +1,15 @@
+//let nibir
+function add(num1,num2){
+     console.log(num1+num2);
+     return
+}
+const result =add(13,14);
+console.log(result);
+
+const premikkk = {name:"smart-dude", phone:52125};
+console.log(premikkk.gf);
+let nibir = undefined;
+
+console.log(nibir);
+
+null
