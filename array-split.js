ccb49edc8b3836dad12440bc1545b6cc24c97c3e
nibir-code <mubtashimfuadnibir@gmail.com>
2022-01-06 03:48:45 +0600

    array-split.js

---
+++ array-split.js
@@ -0,0 +1,6 @@
+const num = [1,2,4,5,6,8,2];
+
+const remove = num.slice(1,7);
+const remove = num.splice(1,7);
+const remove = num.join(','); 
+console.log(remove);
--- double vs triple-equal.js
+++ double vs triple-equal.js
@@ -1,6 +1,6 @@
 const num1 = 20;
 const num2 = false;
-if (num2 === num1){        //=== check the type so use for compare
+if (num2 === num1){        //=== check the type so use for compare .
      console.log("true");
 }
 else{
